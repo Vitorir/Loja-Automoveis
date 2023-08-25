02715d16e5607f81a3a71d96151c24237aa9188b
Vitor Ipiranga <100983862+Vitorir@users.noreply.github.com>
2023-08-25 14:08:37 -0300

    Add files via upload

---
+++ PHP/infos.php
@@ -0,0 +1,7 @@
+<?php
+// Configuração do banco de dados
+$host = "banco-aws.cwi2ranmkgkz.us-east-1.rds.amazonaws.com";
+$username = "admin";
+$password = "39670975123";
+$database = "db_conce";
+?>
